210b56bc184ce000f5fa80f8d6f34db15217ed88
Unknown2526 <an_tho_888@live.ca>
2018-03-22 21:03:22 -0400

    Zone prénom valide avec 50 espaces

--- src/app/probleme/probleme.component.spec.ts
+++ src/app/probleme/probleme.component.spec.ts
@@ -54,4 +54,10 @@ describe('ProblemeComponent', () => {
     errors = zone.errors || {};
     expect(errors['minLength']).toBeFalsy();
   });
+
+  it('Zone prénom valide avec 50 espaces', () => {
+    let zone = component.problemeForm.controls['prenom'];
+    zone.setValue(' '.repeat(50));
+    expect(zone.valid).toBeTruthy();
+  });
 });
