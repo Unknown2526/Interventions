3e801510c9dde40379d3d35b19b6a36068f9b933
Unknown2526 <an_tho_888@live.ca>
2018-04-20 08:59:59 -0400

    Validator est fonctionnel

--- src/app/shared/caracteres-validator.ts
+++ src/app/shared/caracteres-validator.ts
@@ -4,18 +4,22 @@ export class VerifierCaracteresValidator {
     static sansEspaces(): ValidatorFn {
         return (c: AbstractControl): { [key: string]: boolean } | null => {
             if (c.value.trim() == 0) {
-            return { 'plage': false };
+                return { 'plage': false };
             }
-            return { 'plage': true };
+            else {
+                return { 'plage': true };
+            }
         };
     }
 
     static longueurMinimum(min: number): ValidatorFn {
         return (c: AbstractControl): { [key: string]: boolean } | null => {
             if (c.value.trim().length < min) {
-            return { 'plage': false };
+                return { 'plage': false };
             }
-            return { 'plage': true };
+            else {
+                return { 'plage': true };
+            }
         };
     }
 }
